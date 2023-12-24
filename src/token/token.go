5302e92d89085cf75f7d87d14123f44f6200093e
Farouk-Echaref <farouk.echcharef@usmba.ac.ma>
2023-12-24 13:43:05 +0100

    feat: Token struct

---
+++ src/token/token.go
@@ -0,0 +1,14 @@
+package token
+
+//type of token(string can hold many forms of types)
+type TokenType string
+
+//struct for the Token(has a Type and value of the Token)
+type Token struct{
+	Type TokenType
+	Value string
+}
+
+//type of TokenTypes
+
+
